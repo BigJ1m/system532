d65763d66189a44f38be62d68cff75d3879cf80d
Big JIm <bj474006@gmail.com>
2025-05-07 12:19:51 +0800

    Update and rename cloneBSB.command to cloneBSB.sh

--- cloneBSB.sh
+++ cloneBSB.sh
@@ -1,12 +1,13 @@
-cd /users/Shared/system532
-if [ ! -d "/Users/Shared/sytem532/homebrew" ]; then
+#!/bin/bash
+cd /Users/Shared/system532
+if [ ! -d "/Users/Shared/system532/homebrew" ]; then
   echo "Installing Homebrew..."
   mkdir homebrew
   curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 else
   echo "Homebrew already installed."
 fi
-export PATH="/Users/Shared/sytem532/homebrew/bin:$PATH"
+export PATH="/Users/Shared/system532/homebrew/bin:$PATH"
 brew --version
 brew install python
 python3 --version
